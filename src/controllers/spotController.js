c52a4fbd8b624842e8f30466b1dbba7dae90ddef
Stonehagen <steinhagen@mailbox.org>
2023-07-19 14:48:55 +0200

    update spot model

--- src/controllers/spotController.js
+++ src/controllers/spotController.js
@@ -14,3 +14,17 @@ exports.spotGet = async (req, res) => {
     return res.status(400).json({ message: 'Something went wrong' });
   }
 };
+// const getAbsoluteLon = (lonStart, lonEnd) => {
+//   return lonStart > lonEnd ? lonEnd + 360 : lonEnd;
+// };
+
+// const getGribIndex = (forecastInfo, spot) => {
+//   // check if end value for longitute is lower than start value
+//   const lo2 = getAbsoluteLon(forecastInfo.lo1, forecastInfo.lo2);
+//   const spotLon = getAbsoluteLon(forecastInfo.lo1, spot.lon);
+
+//   const latRow = (spot.lat - forecastInfo.la1) / forecastInfo.dy;
+//   const latWidth = (lo2 - forecastInfo.lo1) / forecastInfo.dx + 1;
+//   const lonPos = (spotLon - forecastInfo.lo1) / forecastInfo.dx;
+//   return latRow * latWidth + lonPos;
+// };
