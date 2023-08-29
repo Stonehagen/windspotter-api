f050911668089bd501447e283d6e84b54fe183b2
Stonehagen <steinhagen@mailbox.org>
2023-08-29 17:54:26 +0200

    refactored the spotController

--- src/controllers/spotController.js
+++ src/controllers/spotController.js
@@ -1,45 +1,49 @@
 const { body, validationResult } = require('express-validator');
 const { Spot } = require('../models');
 
+// Helper function to send error response
+const sendError = (res, message) => res.status(400).json({ message });
+
+const nameValidator = body('name')
+  .trim()
+  .isLength({ min: 3 })
+  .withMessage('Spot name must be at least 3 chars long')
+  .isLength({ max: 50 })
+  .withMessage('Spot name cant be no longer than 50 chars')
+  .escape();
+
+const latValidator = body('lat')
+  .trim()
+  .isNumeric()
+  .withMessage('Latitude must be a number')
+  .escape();
+
+const lonValidator = body('lon')
+  .trim()
+  .isNumeric()
+  .withMessage('Longitude must be a number')
+  .escape();
+
 exports.spotListGet = async (req, res) => {
   try {
     const spots = await Spot.find();
-    return res.status(200).json({ spots });
+    res.status(200).json({ spots });
   } catch {
-    return res.status(400).json({ message: 'failed to find any spots' });
+    sendError(res, 'failed to find any spots');
   }
 };
 
 exports.createSpotPost = [
-  body('name')
-    .trim()
-    .isLength({ min: 3 })
-    .withMessage('Spot name must be at least 3 chars long')
-    .isLength({ max: 50 })
-    .withMessage('Spot name cant be no longer than 50 chars')
-    .escape(),
-  body('lat')
-    .trim()
-    .isNumeric()
-    .withMessage('Latitude must be a number')
-    .escape(),
-  body('lon')
-    .trim()
-    .isNumeric()
-    .withMessage('Longitude must be a number')
-    .escape(),
+  nameValidator,
+  latValidator,
+  lonValidator,
   async (req, res) => {
     try {
-      const spot = new Spot({
-        name: req.body.name,
-        lat: req.body.lat,
-        lon: req.body.lon,
-      });
-
+      const spot = new Spot(req.body);
       await spot.save();
-      return res.status(201).json({ spot });
+      res.status(201).json({ spot });
     } catch {
-      return res.status(400).json({ message: 'failed to create new Spot' });
+      sendError(res, 'failed to create new Spot');
     }
   },
 ];
@@ -47,84 +51,57 @@ exports.createSpotPost = [
 exports.spotGet = async (req, res) => {
   try {
     const spot = await Spot.findById(req.params.id);
-
-    if (!spot) {
-      return res.status(400).json({ message: 'Spot not found' });
-    }
-
-    return res.status(200).json({ spot });
+    spot ? res.status(200).json({ spot }) : sendError(res, 'Spot not found');
   } catch {
-    return res.status(400).json({ message: 'failed to find that spot' });
+    sendError(res, 'failed to find that spot');
   }
 };
 
 exports.spotDelete = async (req, res) => {
   try {
     const spot = await Spot.findById(req.params.id);
-
-    if (!spot) {
-      return res.status(400).json({ message: 'Spot not found' });
+    if (spot) {
+      await spot.remove();
+      res.status(200).json({ message: 'Spot deleted' });
+    } else {
+      sendError(res, 'Spot not found');
     }
-
-    await spot.remove();
-    return res.status(200).json({ message: 'Spot deleted' });
   } catch {
-    return res.status(400).json({ message: 'failed to delete spot' });
+    sendError(res, 'failed to delete spot');
   }
 };
 
 exports.spotPut = [
-  body('name')
-    .trim()
-    .isLength({ min: 3 })
-    .withMessage('Spot name must be at least 3 chars long')
-    .isLength({ max: 50 })
-    .withMessage('Spot name cant be no longer than 50 chars')
-    .escape(),
-  body('lat')
-    .trim()
-    .isNumeric()
-    .withMessage('Latitude must be a number')
-    .escape(),
-  body('lon')
-    .trim()
-    .isNumeric()
-    .withMessage('Longitude must be a number')
-    .escape(),
+  nameValidator,
+  latValidator,
+  lonValidator,
   async (req, res) => {
     try {
       const spot = await Spot.findById(req.params.id);
-
-      if (!spot) {
-        return res.status(400).json({ message: 'Spot not found' });
+      if (spot) {
+        Object.assign(spot, req.body);
+        await spot.save();
+        res.status(200).json({ spot });
+      } else {
+        sendError(res, 'Spot not found');
       }
-
-      spot.name = req.body.name;
-      spot.lat = req.body.lat;
-      spot.lon = req.body.lon;
-
-      await spot.save();
-      return res.status(200).json({ spot });
     } catch {
-      return res.status(400).json({ message: 'failed to change spot' });
+      sendError(res, 'failed to change spot');
     }
   },
 ];
 
 exports.SpotForecastGet = async (req, res) => {
   try {
-    const spot = await Spot.findById(req.params.id);
-    populate({
-      path: 'forecasts',
-      populate: { path: 'forecastInfo' },
-    }).exec();
+    const spot = await Spot.findById(req.params.id)
+      .populate({
+        path: 'forecasts',
+        populate: { path: 'forecastInfo' },
+      })
+      .exec();
 
-    if (!spot) {
-      return res.status(400).json({ message: 'Spot not found' });
-    }
-
-    return res.status(200).json({ spot });
+    spot ? res.status(200).json({ spot }) : sendError(res, 'Spot not found');
   } catch {
-    return res.status(400).json({ message: 'failed to find that spot' });
+    sendError(res, 'failed to find that spot');
   }
 };
