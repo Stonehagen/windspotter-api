047f790cdb232423f81ea0924a0730730057cdbb
Stonehagen <steinhagen@mailbox.org>
2023-08-29 15:59:03 +0200

    add routes to spot controller

--- src/controllers/spotController.js
+++ src/controllers/spotController.js
@@ -1,6 +1,14 @@
-/* eslint-disable no-underscore-dangle */
 const { body, validationResult } = require('express-validator');
 const { Spot } = require('../models');
+
+exports.spotListGet = async (req, res) => {
+  try {
+    const spots = await Spot.find();
+    return res.status(200).json({ spots });
+  } catch {
+    return res.status(400).json({ message: 'failed to find any spots' });
+  }
+};
 
 exports.createSpotPost = [
   body('name')
@@ -31,14 +39,14 @@ exports.createSpotPost = [
       await spot.save();
       return res.status(201).json({ spot });
     } catch {
-      return res.status(400).json({ message: 'Something went wrong' });
+      return res.status(400).json({ message: 'failed to create new Spot' });
     }
   },
 ];
 
 exports.spotGet = async (req, res) => {
   try {
-    const spot = await Spot.findById(req.params.postId);
+    const spot = await Spot.findById(req.params.id);
 
     if (!spot) {
       return res.status(400).json({ message: 'Spot not found' });
@@ -46,6 +54,59 @@ exports.spotGet = async (req, res) => {
 
     return res.status(200).json({ spot });
   } catch {
-    return res.status(400).json({ message: 'Something went wrong' });
+    return res.status(400).json({ message: 'failed to find that spot' });
   }
 };
+
+exports.spotDelete = async (req, res) => {
+  try {
+    const spot = await Spot.findById(req.params.id);
+
+    if (!spot) {
+      return res.status(400).json({ message: 'Spot not found' });
+    }
+
+    await spot.remove();
+    return res.status(200).json({ message: 'Spot deleted' });
+  } catch {
+    return res.status(400).json({ message: 'failed to delete spot' });
+  }
+};
+
+exports.spotPut = [
+  body('name')
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage('Spot name must be at least 3 chars long')
+    .isLength({ max: 50 })
+    .withMessage('Spot name cant be no longer than 50 chars')
+    .escape(),
+  body('lat')
+    .trim()
+    .isNumeric()
+    .withMessage('Latitude must be a number')
+    .escape(),
+  body('lon')
+    .trim()
+    .isNumeric()
+    .withMessage('Longitude must be a number')
+    .escape(),
+  async (req, res) => {
+    try {
+      const spot = await Spot.findById(req.params.id);
+
+      if (!spot) {
+        return res.status(400).json({ message: 'Spot not found' });
+      }
+
+      spot.name = req.body.name;
+      spot.lat = req.body.lat;
+      spot.lon = req.body.lon;
+
+      await spot.save();
+      return res.status(200).json({ spot });
+    } catch {
+      return res.status(400).json({ message: 'failed to change spot' });
+    }
+  },
+];
--- src/routes/spot.js
+++ src/routes/spot.js
@@ -3,12 +3,10 @@ const spotController = require('../controllers/spotController');
 
 const router = Router();
 
+router.get('/list', spotController.spotListGet);
 router.post('/new', spotController.createSpotPost);
-
 router.get('/:id', spotController.spotGet);
-
 router.delete('/:id', spotController.spotDelete);
-
 router.put('/:id', spotController.spotPut);
 
 module.exports = router;
